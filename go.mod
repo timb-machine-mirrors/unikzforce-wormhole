761b090a795bf0bc0d678d465cba7a72251f8135
hossein bakhtiari <hossein.bakhtiari@revolut.com>
2024-01-20 18:17:33 +0100

    WRM-1 initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module wormhole
+
+go 1.21
---
+++ main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+
+	got := 2
+	want := 2
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+
+}
+
+func TestAdd22222(t *testing.T) {
+
+	got := 2
+	want := 2
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+
+}
