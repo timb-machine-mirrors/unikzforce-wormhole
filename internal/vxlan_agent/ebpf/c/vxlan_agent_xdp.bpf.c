8608d12fa98d93bdd8e640b3b2d23e2401a4492e
unikzforce <h.bakhtiary.z@gmail.com>
2024-07-15 10:25:21 +0200

    WRM-15 refactor

--- internal/vxlan_agent/ebpf/c/vxlan_agent_xdp.bpf.c
+++ internal/vxlan_agent/ebpf/c/vxlan_agent_xdp.bpf.c
@@ -119,6 +119,7 @@ static __always_inline bool is_broadcast_address(const struct mac_address *mac) 
     return true;
 }
 
+void __always_inline add_headers_to_internal_packet(struct xdp_md *ctx, struct mac_address* dest_mac_addr);
 
 
 SEC("xdp")
@@ -166,78 +167,7 @@ long vxlan_agent_xdp(struct xdp_md *ctx)
                 // if packet need to be forwarded to an internal interface
                 return bpf_redirect(*iface_to_redirect, 0);
             } else {
-                // if packet need to be forwarded to an external interface
-                void *data = (void *)(long)ctx->data;
-                void *data_end = (void *)(long)ctx->data_end;
-                struct ethhdr* inner_eth = data;
-
-                struct ethhdr* outer_eth;
-                struct iphdr* outer_iph;
-                struct udphdr* outer_udph;
-                struct vxlanhdr* outer_vxh;
-
-                // Calculate the new packet length
-                int old_len = data_end - data;
-                int new_len = old_len + NEW_HDR_LEN;
-
-                // Resize the packet buffer by increasing the headroom
-                if (bpf_xdp_adjust_head(ctx, -NEW_HDR_LEN))
-                    return XDP_DROP;
-
-                // Recalculate data and data_end pointers after adjustment
-                data = (void *)(long)ctx->data;
-                data_end = (void *)(long)ctx->data_end;
-
-                 // Ensure the packet is still valid after adjustment
-                if (data + new_len > data_end)
-                    return XDP_DROP;
-
-                outer_eth = data;
-                outer_iph = data + ETH_HLEN;          // ETH_HLEN == 14 & ETH_ALEN == 6
-                outer_udph = (void*) outer_iph + IP_HDR_LEN;
-                outer_vxh = (void*) outer_udph + UDP_HDR_LEN;
-
-                struct in_addr* border_ip = bpf_map_lookup_elem(&mac_to_border_ip_map, &dest_mac_addr);
-
-                if (border_ip == NULL) {
-                    return XDP_ABORTED;
-                }
-
-                struct external_route_info* route_info = bpf_map_lookup_elem(&border_ip_to_route_info_map, border_ip);
-
-                if (route_info == NULL) {
-                    return XDP_ABORTED;
-                }
-
-                //  In network programming, the distinction between the endianness of multi-byte values and single-byte values or byte sequences is critical.
-                //  - Multi-byte Values: The endianness affects how multi-byte values (such as 16-bit, 32-bit, and 64-bit integers) are stored in memory. Network protocols typically require these values to be in big-endian (network byte order) format.
-                //  - Byte Sequences: Single-byte values or sequences of bytes (such as MAC addresses) are not affected by endianness. They are simply copied as they are, byte by byte.
-                __builtin_memcpy(outer_eth->h_source, route_info->external_iface_mac.mac, ETH_ALEN);
-                __builtin_memcpy(outer_eth->h_dest, route_info->external_iface_next_hop_mac.mac, ETH_ALEN);
-                outer_eth->h_proto = bpf_htons(ETH_P_IP);
-
-                outer_iph->version = 4;
-                outer_iph->ihl = 5;
-                outer_iph->tos = 0;
-                outer_iph->tot_len = bpf_htons(new_len - ETH_HLEN);
-                outer_iph->id=0;
-                outer_iph->frag_off = 0;
-                outer_iph->ttl = 64;
-                outer_iph->protocol = IPPROTO_UDP;
-                outer_iph->check = 0; // will be calculated later
-                outer_iph->saddr = bpf_htonl(route_info->external_iface_ip);
-                outer_iph->daddr = bpf_htonl(*border_ip);
-
-
-                outer_udph->source = bpf_htons(get_ephemeral_port()); // Source UDP port
-                outer_udph->dest = bpf_htons(4789); // Destination UDP port (VXLAN default)
-                outer_udph->len = bpf_htons(new_len - ETH_HLEN - IP_HDR_LEN);
-                outer_udph->check = 0; // UDP checksum is optional in IPv4
-
-                // for now we don't set VXLAN header
-
-                // Calculate ip checksum
-                outer_iph->check = ~bpf_csum_diff(0, 0, (__u32)outer_iph, IP_HDR_LEN, 0);
+                add_headers_to_internal_packet(ctx, &dest_mac_addr);
 
                 return bpf_redirect(*iface_to_redirect, 0);
             }
@@ -350,6 +280,80 @@ long vxlan_agent_xdp(struct xdp_md *ctx)
     }
 }
 
+void add_headers_to_internal_packet(struct xdp_md *ctx, struct mac_address* dest_mac_addr) {
+    // if packet need to be forwarded to an external interface
+    void *data = (void *)(long)ctx->data;
+    void *data_end = (void *)(long)ctx->data_end;
+    struct ethhdr* inner_eth = data;
+
+    struct ethhdr* outer_eth;
+    struct iphdr* outer_iph;
+    struct udphdr* outer_udph;
+    struct vxlanhdr* outer_vxh;
+
+    // Calculate the new packet length
+    int old_len = data_end - data;
+    int new_len = old_len + NEW_HDR_LEN;
+
+    // Resize the packet buffer by increasing the headroom
+    if (bpf_xdp_adjust_head(ctx, -NEW_HDR_LEN))
+        return XDP_DROP;
+
+    // Recalculate data and data_end pointers after adjustment
+    data = (void *)(long)ctx->data;
+    data_end = (void *)(long)ctx->data_end;
+
+        // Ensure the packet is still valid after adjustment
+    if (data + new_len > data_end)
+        return XDP_DROP;
+
+    outer_eth = data;
+    outer_iph = data + ETH_HLEN;          // ETH_HLEN == 14 & ETH_ALEN == 6
+    outer_udph = (void*) outer_iph + IP_HDR_LEN;
+    outer_vxh = (void*) outer_udph + UDP_HDR_LEN;
+
+    struct in_addr* border_ip = bpf_map_lookup_elem(&mac_to_border_ip_map, dest_mac_addr);
+
+    if (border_ip == NULL) {
+        return XDP_ABORTED;
+    }
+
+    struct external_route_info* route_info = bpf_map_lookup_elem(&border_ip_to_route_info_map, border_ip);
+
+    if (route_info == NULL) {
+        return XDP_ABORTED;
+    }
+
+    //  In network programming, the distinction between the endianness of multi-byte values and single-byte values or byte sequences is critical.
+    //  - Multi-byte Values: The endianness affects how multi-byte values (such as 16-bit, 32-bit, and 64-bit integers) are stored in memory. Network protocols typically require these values to be in big-endian (network byte order) format.
+    //  - Byte Sequences: Single-byte values or sequences of bytes (such as MAC addresses) are not affected by endianness. They are simply copied as they are, byte by byte.
+    __builtin_memcpy(outer_eth->h_source, route_info->external_iface_mac.mac, ETH_ALEN);
+    __builtin_memcpy(outer_eth->h_dest, route_info->external_iface_next_hop_mac.mac, ETH_ALEN);
+    outer_eth->h_proto = bpf_htons(ETH_P_IP);
+
+    outer_iph->version = 4;
+    outer_iph->ihl = 5;
+    outer_iph->tos = 0;
+    outer_iph->tot_len = bpf_htons(new_len - ETH_HLEN);
+    outer_iph->id=0;
+    outer_iph->frag_off = 0;
+    outer_iph->ttl = 64;
+    outer_iph->protocol = IPPROTO_UDP;
+    outer_iph->check = 0; // will be calculated later
+    outer_iph->saddr = bpf_htonl(route_info->external_iface_ip);
+    outer_iph->daddr = bpf_htonl(*border_ip);
+
+
+    outer_udph->source = bpf_htons(get_ephemeral_port()); // Source UDP port
+    outer_udph->dest = bpf_htons(4789); // Destination UDP port (VXLAN default)
+    outer_udph->len = bpf_htons(new_len - ETH_HLEN - IP_HDR_LEN);
+    outer_udph->check = 0; // UDP checksum is optional in IPv4
+
+    // for now we don't set VXLAN header
+
+    // Calculate ip checksum
+    outer_iph->check = ~bpf_csum_diff(0, 0, (__u32)outer_iph, IP_HDR_LEN, 0);
+}
 
 void learn_internal_source_host(const struct xdp_md *ctx, const struct ethhdr *eth, __u64 current_time)
 {
