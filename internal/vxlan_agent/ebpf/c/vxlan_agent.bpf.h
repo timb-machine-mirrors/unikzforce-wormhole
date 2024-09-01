d7b8b202cfcf291fe983d7bdd0470036c3e030fc
unikzforce <h.bakhtiary.z@gmail.com>
2024-09-01 19:56:14 +0200

    WRM-15 improve code

--- internal/vxlan_agent/ebpf/c/vxlan_agent.bpf.h
+++ internal/vxlan_agent/ebpf/c/vxlan_agent.bpf.h
@@ -6,27 +6,26 @@
 #include <bpf/bpf_endian.h>
 #include <bpf/bpf_helpers.h>
 
-
 #define TC_ACT_SHOT 2
 #define TC_ACT_OK 0
 
-#define ETH_P_IP 0x0800 /* Internet Protocol packet */
-#define ETH_ALEN 6      /* Ethernet address length */
+#define ETH_P_IP 0x0800  /* Internet Protocol packet */
+#define ETH_ALEN 6       /* Ethernet address length */
 #define ETH_P_ARP 0x0806 /* Address Resolution packet */
 #define ARPOP_REQUEST 1  /* ARP request */
 #define ARPOP_REPLY 2    /* ARP reply */
 #define CLOCK_BOOTTIME 7 /* Monotonic system-wide clock that includes time spent in suspension.  */
-#define ETH_HLEN 14 /* Total octets in header.	 */
+#define ETH_HLEN 14      /* Total octets in header.	 */
 #define IP_HDR_LEN (int)sizeof(struct iphdr)
 #define UDP_HDR_LEN (int)sizeof(struct udphdr)
 #define VXLAN_HDR_LEN (int)sizeof(struct vxlanhdr)
 #define NEW_HDR_LEN (ETH_HLEN + IP_HDR_LEN + UDP_HDR_LEN + VXLAN_HDR_LEN)
 
-
-enum vxlan_agent_processing_error {
-	AGENT_ERROR_ABORT = 0,
-	AGENT_ERROR_DROP = 1,
-	AGENT_NO_ERROR = 2,
+enum vxlan_agent_processing_error
+{
+    AGENT_ERROR_ABORT = 0,
+    AGENT_ERROR_DROP = 1,
+    AGENT_NO_ERROR = 2,
 };
 
 struct external_route_info
