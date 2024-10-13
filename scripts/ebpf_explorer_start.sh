5aa8a23b3e282bfb9d2cbb02e23a54819cf31fa6
unikzforce <h.bakhtiary.z@gmail.com>
2024-10-13 21:10:42 +0200

    WRM-18 improve

--- scripts/ebpf_explorer_start.sh
+++ scripts/ebpf_explorer_start.sh
@@ -1,4 +1,19 @@
-docker run -ti --rm -p 8070:80 \
+#!/bin/bash
+
+# Try running the ARM64 image first
+echo "Trying to run the ARM64 image..."
+docker run --platform linux/arm64 -ti --rm -p 8070:80 \
   --cap-add CAP_SYS_ADMIN --pid=host \
   -e BPF_DIR=/sys/fs/bpf -v /sys/fs/bpf:/sys/fs/bpf \
   ghcr.io/ebpfdev/explorer:v0.0.7
+
+# Check if the ARM64 image is not available
+if [ $? -ne 0 ]; then
+  echo "ARM64 image not found. Trying to run the AMD64 image on Apple Silicon (with emulation)..."
+  
+  # Fallback to running the AMD64 image
+  docker run --platform linux/amd64 -ti --rm -p 8070:80 \
+    --cap-add CAP_SYS_ADMIN --pid=host \
+    -e BPF_DIR=/sys/fs/bpf -v /sys/fs/bpf:/sys/fs/bpf \
+    ghcr.io/ebpfdev/explorer:v0.0.7
+fi
