8732736fd50b57bb0faafbf8a9c1b6b8d7f27f3d
unikzforce <h.bakhtiary.z@gmail.com>
2024-10-21 01:22:18 +0200

    WRM-18 nothing

--- scripts/ebpf_explorer_start.sh
+++ scripts/ebpf_explorer_start.sh
@@ -2,7 +2,7 @@
 
 # Try running the ARM64 image first
 echo "Trying to run the ARM64 image..."
-docker run --platform linux/arm64 -ti --rm -p 8070:80 \
+docker run -ti --rm -p 8070:80 \
   --cap-add CAP_SYS_ADMIN --pid=host \
   -e BPF_DIR=/sys/fs/bpf -v /sys/fs/bpf:/sys/fs/bpf \
   ghcr.io/ebpfdev/explorer:v0.0.7
